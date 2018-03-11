8d8a8f13553c5f5a23ea5c2e1f2a309bc350b23a
Jonathan Blair <sev9780@gmail.com>
2018-03-10 19:31:20 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -18,8 +18,34 @@ function filter_ptags($content){
 
 add_filter('the_content', 'filter_ptags');
 
-function signup_newsletter( $data ) {
-  return 'hello';
+function signup_newsletter($data) {
+    $apiKey = 'api key here';
+    $listId = 'list id here';
+
+    $memberId = md5(strtolower($data['email']));
+    $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+    $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
+
+    $json = json_encode([
+        'email_address' => $data['email'],
+        'status'        => $data['status'], // "subscribed","unsubscribed","cleaned","pending"
+    ]);
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);                                                                                                                 
+
+    $result = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return $httpCode;
 }
 
 add_action('rest_api_init', function() {
