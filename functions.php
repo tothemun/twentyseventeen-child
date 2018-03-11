08ce99a44cf14febbeff3637f978c05cef941f49
Jonathan Blair <sev9780@gmail.com>
2018-03-10 19:18:05 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,3 +3,17 @@
  function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  }
+
+add_post_type_support( 'page', 'excerpt' );
+
+function filter_ptags($content){
+  $content = preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+  $content = preg_replace('/<p>\s*(<script.*>*.<\/script>)\s*<\/p>/iU', '\1', $content);
+  $content = preg_replace_callback('/<p>\s*(<iframe.*>*.<\/iframe>)\s*<\/p>/iU', function($matches) {
+        return '<div class="embedContainer">' . $matches[0] . '</div>';
+  }, $content);
+	
+  return $content;
+}
+
+add_filter('the_content', 'filter_ptags');
